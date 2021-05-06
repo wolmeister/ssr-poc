b1a2eac8813b28877c6dc759a5888ac01583dd79
Victor Wolmeister <wolmeistervictor@gmail.com>
2021-05-06 19:26:14 -0300

    Add angular universal application

--- angular-universal/src/app/app.component.ts
+++ angular-universal/src/app/app.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+type User = {
+  name: string;
+  avatarUrl: string;
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  public user: User | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<User>('http://localhost:3000/api/users/me').subscribe(user => {
+      this.user = user;
+    });
+  }
+}
